d74280394f39800741baebf8cc97910bede2f6c1
Andrey Mishchenko <ant1kguild@gmail.com>
2020-07-02 02:52:59 +0300

    refactoring: migrate to kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+buildscript {
+
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath(Gradle.android_tools)
+        classpath(Gradle.kotlin_plugin)
+        classpath(Gradle.android_safe_arg_plugin)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven{
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+tasks.register("clean", Delete::class.java){
+    delete(rootProject.buildDir)
+}
