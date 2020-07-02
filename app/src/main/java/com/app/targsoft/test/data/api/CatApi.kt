c48d939d8d7e9ac0db231e7f598f0e131a1998db
Andrey Mishchenko <ant1kguild@gmail.com>
2020-07-02 17:49:04 +0300

    add: CatApi interface

---
+++ app/src/main/java/com/app/targsoft/test/data/api/CatApi.kt
@@ -0,0 +1,30 @@
+package com.app.targsoft.test.data.api
+
+import com.app.targsoft.test.data.model.Cat
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Inject
+
+interface CatApi {
+    @GET("images/search")
+    suspend fun getPaginationCats(
+        @Query("limit") limit: Int,
+        @Query("page") page: Int,
+        @Query("order") order: String
+    ): List<Cat>
+}
+
+interface CatApiHelper {
+    suspend fun getPaginationCats(
+        limit: Int,
+        page: Int,
+        order: String
+    ): List<Cat>
+}
+
+class CatApiHelperImpl @Inject constructor(private val api: CatApi) : CatApiHelper {
+    override suspend fun getPaginationCats(limit: Int, page: Int, order: String): List<Cat> {
+        return api.getPaginationCats(limit, page, order)
+    }
+
+}
