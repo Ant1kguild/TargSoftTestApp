f8157f10d7cad6619b64aa742f6047b9682aa914
Andrey Mishchenko <ant1kguild@gmail.com>
2020-07-02 18:40:33 +0300

    add: FavoriteCarDao

---
+++ app/src/main/java/com/app/targsoft/test/data/datasource/database/FavoriteCarDao.kt
@@ -0,0 +1,23 @@
+package com.app.targsoft.test.data.datasource.database
+
+import androidx.room.*
+import com.app.targsoft.test.data.datasource.model.FavoriteCat
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteCarDao {
+    @Query(value = "SELECT * FROM cat_table")
+    fun getAllFavoriteCats(): Flow<List<FavoriteCat>>
+
+    @Query(value = "SELECT * from cat_table WHERE id=:id")
+    fun getFavoriteCat(id: Int): Flow<FavoriteCat>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(cat: FavoriteCat)
+
+    @Delete
+    suspend fun deleteAnimal(cat: FavoriteCat)
+
+    @Query(value = "DELETE FROM cat_table")
+    suspend fun deleteAll()
+}
