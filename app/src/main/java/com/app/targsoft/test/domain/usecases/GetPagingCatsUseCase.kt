152bffa6d4bc558f62f8eeb912c2883f90ab47e8
Andrey Mishchenko <ant1kguild@gmail.com>
2020-07-03 20:37:53 +0300

    add: GetPagingCatsUseCase.kt

--- app/src/main/java/com/app/targsoft/test/domain/usecases/GetPagingCatsUseCase.kt
+++ app/src/main/java/com/app/targsoft/test/domain/usecases/GetPagingCatsUseCase.kt
@@ -1,12 +1,12 @@
 package com.app.targsoft.test.domain.usecases
 
 import androidx.paging.PagingData
-import com.app.targsoft.test.data.api.model.Cat
+import com.app.targsoft.test.data.datasource.database.model.FavoriteCat
 import com.app.targsoft.test.domain.repository.CatRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetPagingCatsUseCase(
     private val catRepository: CatRepository
-) : BaseUseCaseWithOneParam<String, PagingData<Cat>> {
-    override fun exec(param: String): Flow<PagingData<Cat>> = catRepository.getAllCatsStream(param)
+) : BaseUseCaseWithOneParam<String, PagingData<FavoriteCat>> {
+    override fun exec(param: String): Flow<PagingData<FavoriteCat>> = catRepository.getAllCatsStream(param)
 }
